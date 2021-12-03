a983445d0c2319f44fcd2242f94abc9c8ebbe5e9
etopbrandon <brandon@etoptechnology.com>
2021-12-03 11:52:07 -0800

    Updated script to handle exits more gracefully

--- MailboxPermissions.ps1
+++ MailboxPermissions.ps1
@@ -17,18 +17,23 @@ if ($PermissionSel -eq "1"){
         Get-MailboxPermission -Identity $Identity -User $User
         Get-RecipientPermission -Identity $Identity
         Read-Host "I have made the above changes. Press enter to exit"
+        Get-PSSession | Remove-PSSession
     }else{
         Read-host "Press enter to exit"
+        Get-PSSession | Remove-PSSession
     }
 } elseif ($PermissionSel -eq "2") {
+    $Confirm = Read-Host "I will be granting $user read only access to the $identity mailbox. Would you like me to proceed? (y/n)"
     if ($Confirm -eq "y"){
         Connect-ExchangeOnline
         Write-Host "Updating permissions, please wait..."
         Add-MailboxPermission -Identity $Identity -User $User -AccessRights ReadPermission -InheritanceType All -AutoMapping $TRUE | Out-Null
         Get-MailboxPermission -Identity $Identity -User $User
         Read-Host "I have made the above changes. Press enter to exit"
+        Get-PSSession | Remove-PSSession
     }else{
         Read-host "Press enter to exit"
+        Get-PSSession | Remove-PSSession
     }
 } else {
     Read-Host "Invalid entry, press enter to exit"
--- README.md
+++ README.md
@@ -2,4 +2,7 @@
 Update Exchange Online Mailbox Permissions
 
 # What does this do?
-This script will update a user's mailbox to allow another user within the organization to have either full editing with sending permissions, or allow read only permissions. It will also Automap the mailbox with Outlook AutoDiscover
+This script will update a user's mailbox to allow another user within the organization to have either full editing with sending permissions, or allow read only permissions. It will also Automap the mailbox. The user will need to add the "From" field and send as addresses if it is not enabled in Outlook.
+
+# Current known issues
+Microsoft has a limit of 3 concurrent sessions at once in PowerShell. Functions are included to end PSSession at the end of execution, but you may experience a "Fail to create a runspace" error if running this script several times. Get-PSSession | Remove-PSSession should kill things that need to be killed at the end, please let me know if you get this error still. 
